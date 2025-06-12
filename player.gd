b26288cd212646a4aa0a0c4e84e707b866b6f714
githubc00l <stellarisabatingdefense107@gmail.com>
2025-06-12 13:20:35 -0700

    test

---
+++ player.gd
@@ -0,0 +1,31 @@
+extends CharacterBody2D
+
+var direction : Vector2 = Vector2.ZERO
+@onready var animation_tree: AnimationTree = $AnimationTree
+
+func _physics_process(_delta):
+	
+	if Input.is_action_pressed("ui_run"):
+		velocity = direction * 150
+		move_and_slide()
+	else:
+		velocity = direction * 75
+		move_and_slide()
+		
+
+func _process(_delta):
+	direction = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
+	
+	if direction != Vector2.ZERO:
+		set_walking(true)
+		update_blend_position()
+	else:
+		set_walking(false)
+		
+func set_walking(value):
+	animation_tree["parameters/conditions/is_walking"] = value
+	animation_tree["parameters/conditions/idle"] = not value
+
+func update_blend_position():
+	animation_tree["parameters/Idle/blend_position"] = direction
+	animation_tree["parameters/walk/blend_position"] = direction
